bb6666d8eb9976597d84c311a8bb74bfadb7f464
Bobby Norton <bobby@testedminds.com>
2014-05-21 13:08:20 -0500

    Remove anything not related to initial setup

--- rabbitmq-tutorials-master/clojure/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject com.rabbitmq/tutorials "1.0.0-SNAPSHOT"
-  :description "RabbitMQ tutorials using Langohr"
-  :url "http://github.com/rabbitmq/rabbitmq-tutorial"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [com.novemberain/langohr "2.3.2"]])
