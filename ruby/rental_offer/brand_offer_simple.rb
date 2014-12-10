dc319f7cff7c5189d7d19db432bffcae72afa5c8
Wirianto Djunaidi <Wirianto.Djunaidi@solnet.co.nz>
2014-12-10 13:24:31 +1300

    add simple brand offer and local offer solutions provider

---
+++ ruby/rental_offer/brand_offer_simple.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require_relative 'connection'
+require 'json'
+
+# Streams rental-offer-related requests to the console
+class BrandOfferSimple
+
+  def initialize(host, bus_name)
+    @host = host
+    @bus_name = bus_name
+  end
+
+  def start
+    Connection.with_open(@host, @bus_name) {|ch, ex| brand_offer_solutions(ch, ex) }
+  end
+
+private
+
+  def brand_offer_solutions(channel, exchange)
+    queue = channel.queue("", :exclusive => true)
+    queue.bind exchange
+    puts " [*] Waiting for need on the '#{@bus_name}' bus... To exit press CTRL+C"
+    queue.subscribe(block: true) do |delivery_info, properties, body|
+      puts " [x] Received #{body}"
+      need = JSON.parse body
+      if need['solutions'].size == 0
+        need['solutions'] << {:json_class => 'BrandOfferSimple', :offer => '5%-off', :value => rand(100) }
+        need_json = JSON.generate need
+        puts " [>] Respond with #{need_json}"
+        exchange.publish need_json
+      end
+    end
+  end
+
+end
+
+BrandOfferSimple.new(ARGV.shift, ARGV.shift).start
